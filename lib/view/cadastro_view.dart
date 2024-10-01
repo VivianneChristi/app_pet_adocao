83cc5d939bdf3403dfdd34e544d069e1ec438aa1
Vivianne Christi <61090532+VivianneChristi@users.noreply.github.com>
2024-10-01 16:40:40 -0300

    a

--- lib/view/cadastro_view.dart
+++ lib/view/cadastro_view.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:pet_adoption/widgets/custom_text_field.dart';
 
 class CadastroView extends StatelessWidget {
+  final _formKey =
+      GlobalKey<FormState>(); // Adicionando uma chave para o formulário
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,33 +17,56 @@ class CadastroView extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Crie sua conta',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            CustomTextField(label: 'Nome'),
-            SizedBox(height: 16),
-            CustomTextField(label: 'E-mail'),
-            SizedBox(height: 16),
-            CustomTextField(label: 'Senha', obscureText: true),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Lógica para cadastro
-                },
-                child: Text('Cadastrar'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+        child: Form(
+          key: _formKey, // Usando a chave para o formulário
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Crie sua conta',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              CustomTextField(
+                label: 'Nome',
+                controller: _nameController, // Passando o controlador
+              ),
+              SizedBox(height: 16),
+              CustomTextField(
+                label: 'E-mail',
+                controller: _emailController, // Passando o controlador
+              ),
+              SizedBox(height: 16),
+              CustomTextField(
+                label: 'Senha',
+                obscureText: true,
+                controller: _passwordController, // Passando o controlador
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Se o formulário for válido, mostre um SnackBar
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Cadastro realizado com sucesso!'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                      // Aqui você pode adicionar a lógica para salvar os dados
+                      // E redirecionar para outra tela, se necessário
+                    }
+                  },
+                  child: Text('Cadastrar'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -3,12 +3,18 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final String label;
   final bool obscureText;
+  final TextEditingController controller; // Controlador
 
-  const CustomTextField({required this.label, this.obscureText = false});
+  const CustomTextField({
+    required this.label,
+    this.obscureText = false,
+    required this.controller, // Adicionando o controlador ao construtor
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      controller: controller, // Usando o controlador
       obscureText: obscureText,
       decoration: InputDecoration(
         labelText: label,
@@ -16,6 +22,20 @@ class CustomTextField extends StatelessWidget {
           borderRadius: BorderRadius.circular(12),
         ),
       ),
+      validator: (value) {
+        // Validação dos campos
+        if (value == null || value.isEmpty) {
+          return 'Por favor, preencha este campo';
+        }
+        if (label == 'E-mail' &&
+            !RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+          return 'Por favor, insira um e-mail válido';
+        }
+        if (label == 'Senha' && value.length < 6) {
+          return 'A senha deve ter pelo menos 6 caracteres';
+        }
+        return null;
+      },
     );
   }
 }
